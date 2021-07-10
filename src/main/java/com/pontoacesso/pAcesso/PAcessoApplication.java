e796d22940fffbccacc6dba994e214073208fe6c
DevOps.Léo Albergaria <63436406+Leo-Albergaria@users.noreply.github.com>
2021-07-10 12:44:04 -0300

    Add files via upload

---
+++ src/main/java/com/pontoacesso/pAcesso/PAcessoApplication.java
@@ -0,0 +1,13 @@
+package com.pontoacesso.pAcesso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PAcessoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PAcessoApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/pontoacesso/pAcesso/model/JornadaTrabalho.java
@@ -0,0 +1,14 @@
+package com.pontoacesso.pAcesso.model;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@Builder
+public class JornadaTrabalho {
+    private long id;
+    private String descricao;
+}

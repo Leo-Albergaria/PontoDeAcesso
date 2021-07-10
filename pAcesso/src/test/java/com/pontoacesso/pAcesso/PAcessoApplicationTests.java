cb86dd98ca1fe9a357a3984c170fc20600a0d373
DevOps.Léo Albergaria <63436406+Leo-Albergaria@users.noreply.github.com>
2021-07-10 09:05:04 -0300

    Add files via upload

---
+++ pAcesso/src/test/java/com/pontoacesso/pAcesso/PAcessoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pontoacesso.pAcesso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PAcessoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
